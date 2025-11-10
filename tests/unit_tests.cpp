3ae74e7ead152dfe47e5afddb06785661d3ff16c
Podryez Nikita <nikitapodryez@gmail.com>
2025-11-10 18:07:31 +0200

    Add unit tests for addition function

---
+++ tests/unit_tests.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "math_operations.h"
+
+TEST_CASE("Addition works correctly", "[add]") {
+    REQUIRE(add(2, 3) == 5);
+    REQUIRE(add(-1, 1) == 0);
+    REQUIRE(add(0, 0) == 0);
+    REQUIRE(add(-5, -7) == -12);
+    REQUIRE(add(1000, 2000) == 3000);
+}
